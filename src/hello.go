700d38139575a0b553a854fd73efdde4909c92d2
Rasmus Lystrøm <rasmus@lystroem.dk>
2026-01-07 20:45:39 +0000

    Add French and Spanish

--- src/hello.go
+++ src/hello.go
@@ -2,13 +2,35 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "¡Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return fmt.Sprintf("Hello, %s!", name)
+
+	return greetingPrefix(language) + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello(""))
+	fmt.Println(Hello("", ""))
 }
